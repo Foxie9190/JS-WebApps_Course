d976361b3d68042d81ef553daec20a19ffb930d1
= <=>
2026-04-21 02:11:46 -0500

    Second Project

---
+++ Dog Years/main.js
@@ -0,0 +1,11 @@
+let MyAge = 16;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = MyAge -= 2;
+laterYears *= 4;
+const MyAgeInDogYears = earlyYears + laterYears;
+
+let Myname = "Landon".toLocaleLowerCase()
+
+
+console.log(`My name is ${Myname}. I am ${MyAge} years old in human years, i am ${MyAgeInDogYears} years old in dog years`)
