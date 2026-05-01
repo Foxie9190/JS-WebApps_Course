6ef89f4ab8a1ce48c1f550f36e6af88fe100abb7
= <=>
2026-05-01 11:33:15 -0500

    New Project

--- Dog Years/main.js
+++ Dog Years/main.js
@@ -1,11 +1,12 @@
 let MyAge = 16;
 let earlyYears = 2;
 earlyYears *= 10.5;
-let laterYears = MyAge -= 2;
+let laterYears = (MyAge -= 2);
 laterYears *= 4;
 const MyAgeInDogYears = earlyYears + laterYears;
 
-let Myname = "Landon".toLocaleLowerCase()
+let Myname = "Landon".toLocaleLowerCase();
 
-
-console.log(`My name is ${Myname}. I am ${MyAge} years old in human years, i am ${MyAgeInDogYears} years old in dog years`)
+console.log(
+  `My name is ${Myname}. I am ${MyAge} years old in human years, i am ${MyAgeInDogYears} years old in dog years`,
+);
---
+++ Magic8Ball/Main.js
@@ -0,0 +1,29 @@
+
+
+var username = `Landon`;
+username ? console.log(`Hello, ${username}`) : console.log("Hello!");
+
+var UserQuestion = `Will I get better soon`;
+
+console.log(UserQuestion);
+
+
+
+var EightBall = Math.floor(Math.random() * 8);
+if (EightBall == 1) {
+    console.log('It is certain') }
+ else if (EightBall == 2) {
+console.log('It is decidedly so') 
+} else if (EightBall == 3) {
+    console.log('Reply hazy try again')
+} else if (EightBall == 4) {
+    console.log('Cannot predict now')
+} else if (EightBall == 5) {
+    console.log(`Do not count on it`)
+} else if (EightBall == 6) {
+    console.log(`my sources say no`)
+} else if (EightBall == 7) {
+    console.log(`Outlook not so good`)
+} else if (EightBall == 8) {
+    console.log(`Signs point to yes`)
+}
